f0bf1b8578a98fdea84a29261d8cb5c8832904a6
jthicks91 <jordanthicks91@gmail.com>
2019-04-06 10:28:49 -0700

    MVP Completed. Displayed smurf data via using get request. Added the addSmurf function via post in SmurfForm and added Routes as well. Moving to stretch now.

--- village/src/App.js
+++ village/src/App.js
@@ -1,27 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+import axios from "axios";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+const URL = "http://localhost:3333/smurfs";
 
-class App extends Component {
+export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+
+  componentDidMount() {
+    console.log("CDM now running");
+    axios
+      .get(URL)
+      .then(response => {
+        console.log("get response", response);
+        this.setState({ smurfs: response.data });
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleData = data => {
+    console.log(data, "data");
+    this.setState({ smurfs: data });
+  };
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+        <SmurfForm handleData={this.handleData} />
+        <Route
+          path="/"
+          render={props => (
+            <Smurfs
+              {...props}
+              smurfs={this.state.smurfs}
+              handleData={this.handleData}
+            />
+          )}
+        />
       </div>
     );
   }
 }
-
-export default App;
