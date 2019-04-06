bd8d4f5819a2fcaee3dad82b118428d0a8c81410
jthicks91 <jordanthicks91@gmail.com>
2019-04-06 11:05:32 -0700

    going back and modularizing for stretch as well as adding some title and actual images of the smurfs to display with edit and delete functions.

--- village/src/App.js
+++ village/src/App.js
@@ -2,8 +2,14 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import SmurfForm from "./components/SmurfForm";
-import Smurfs from "./components/Smurfs";
 import axios from "axios";
+
+import AddSmurfForm from "./components/AddSmurfForm";
+import UpdateSmurfForm from "./components/UpdateSmurfForm";
+import SingleSmurf from "./components/SingleSmurf";
+import Smurfs from "./components/Smurfs";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
 
 const URL = "http://localhost:3333/smurfs";
 
@@ -30,23 +36,66 @@ export default class App extends Component {
     console.log(data, "data");
     this.setState({ smurfs: data });
   };
+
+  updateSmurfs = newSmurfs => {
+    this.setState({ smurfs: newSmurfs });
+  };
+
+  smurfFilename = name => {
+    let modifiedName = name.toLowerCase().replace(" ", "-");
+    return modifiedName + ".png";
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm handleData={this.handleData} />
-        <Route
-          path="/"
-          render={props => (
-            <Smurfs
-              {...props}
-              smurfs={this.state.smurfs}
-              handleData={this.handleData}
-            />
-          )}
-        />
+        <Route path="/" component={Nav} />
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route
+            exact
+            path="/smurfs"
+            render={props => (
+              <Smurfs
+                {...props}
+                smurfs={this.state.smurfs}
+                smurfFilename={this.smurfImgFilename}
+              />
+            )}
+          />
+          <Route
+            exact
+            path="/add-smurf"
+            render={props => (
+              <AddSmurfForm {...props} updateSmurfs={this.updateSmurfs} />
+            )}
+          />
+          <Route
+            exact
+            path="/smurfs/:id"
+            render={props => (
+              <SingleSmurf
+                {...props}
+                updateSmurfs={this.updateSmurfs}
+                smurfs={this.state.smurfs}
+                smurfFilename={this.smurfImgFilename}
+              />
+            )}
+          />
+          <Route
+            path="/smurfs/:id/update"
+            render={props => (
+              <UpdateSmurfForm
+                {...props}
+                updateSmurfs={this.updateSmurfs}
+                smurfs={this.state.smurfs}
+              />
+            )}
+          />
+        </main>
       </div>
     );
   }
